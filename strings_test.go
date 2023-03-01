c967a7bc9140032846067f128e3e061c0c653138
vesic <dusan.vesic@gmail.com>
2023-03-01 19:13:28 +0100

    reverse the string

--- strings_test.go
+++ strings_test.go
@@ -2,9 +2,18 @@ package strings
 
 import "testing"
 
-func TestAbs(t *testing.T) {
+func TestSwapCase(t *testing.T) {
 	got := SwapCase("hello")
 	if got != "HELLO" {
 		t.Errorf("SwapCase(\"hello\") = %s; want \"HELLO\"", got)
+	}
+}
+
+// Test case for the Reverse function
+func TestReverse(t *testing.T) {
+	input, expected := "Hello, World", "dlroW ,olleH"
+	result := Reverse(input)
+	if result != expected {
+		t.Errorf("Reverse(%q) == %q, expected %q", input, result, expected)
 	}
 }
